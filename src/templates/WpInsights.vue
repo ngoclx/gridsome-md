243fa237cae2b2a6dd8a1e306682db176eb8c655
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-08-11 16:22:03 +0700

    Insights

---
+++ src/templates/WpInsights.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <div>
+      <h1 v-html="$page.wpInsights.title" />
+
+      <div v-html="$page.wpInsights.content" />
+    </div>
+</Layout>
+</template>
+
+<page-query>
+query WpInsights ($path: String!) {
+  wpInsights (path: $path) {
+    id
+    title
+    slug
+    path
+    excerpt
+    content
+  }
+}
+</page-query>
+
+<script>
+// import InsightsRelated from "~/components/InsightsRelated.vue";
+
+export default {
+  name: "WpInsights",
+  components: {
+    // InsightsRelated,
+  },
+  mounted() {
+    console.log(this.$page.wpInsights)
+  }
+};
+</script>
