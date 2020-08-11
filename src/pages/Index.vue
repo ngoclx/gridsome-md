24c77fd2b92bcd307acc7c7002ed3f0402964197
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-08-11 14:33:31 +0700

    Homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,32 @@
 <template>
   <Layout>
+    <div>
+      <h1 v-html="$page.wpPage.title" />
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+      <div v-html="$page.wpPage.content" />
+    </div>
+</Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+query WpPage {
+  wpPage (id: 8) {
+    id
+    title
+    slug
+    path
+    excerpt
+    content
   }
 }
-</script>
+</page-query>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+<script>
+
+export default {
+  name: "WpPage",
+  created() {
+    console.log(this.$page.wpPage)
+  }
+};
+</script>
