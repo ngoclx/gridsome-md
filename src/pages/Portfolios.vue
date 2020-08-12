6c2f621666733952f7706f991408e3b161578bfc
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-08-12 21:20:19 +0700

    Case studies component

---
+++ src/pages/Portfolios.vue
@@ -0,0 +1,68 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Case studies</h1>
+
+      <ul class="row post-list">
+        <li
+          v-for="item in this.$page.portfolios.edges"
+          :key="item.id"
+          class="col-md-6 col-xl-4 post-card"
+        >
+          <portfolio-teaser :node="item.node" />
+        </li>
+      </ul>
+
+      <pager
+        :info="this.$page.portfolios.pageInfo"
+        linkClass="page-link rounded"
+        class="pagination justify-content-center"
+      />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query allWpPortfolio($page: Int) {
+  portfolios: allWpPortfolio(perPage: 12, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        slug
+        title
+        path
+        excerpt
+        featuredMedia {
+          id
+          sourceUrl
+          altText
+          title
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import PortfolioTeaser from "~/components/PortfolioTeaser.vue";
+import { Pager } from "gridsome";
+
+export default {
+  components: {
+    PortfolioTeaser,
+    Pager,
+  },
+  metaInfo() {
+    return {
+      title: "Case studies",
+    };
+  },
+  mounted() {
+    console.log(this.$page.portfolios);
+  },
+};
+</script>
