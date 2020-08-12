e4fb75005fcb563e091235895efd83946aef5cbf
Nguyen Ngoc Vinh <ngocvinh@DESKTOP-7K2GITQ.localdomain>
2020-08-12 13:09:24 +0700

    fixes style menu

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -33,6 +33,9 @@ import Submenu from "~/components/Submenu.vue";
 
 export default {
   name: "MenuItem",
+  data:{
+    isHovering: false
+  },
   components: {
     Submenu
   },
@@ -69,3 +72,22 @@ export default {
   // },
 };
 </script>
+<style>
+  #mega-menu-wrap-primary #mega-menu-primary>li.mega-menu-megamenu.mega-menu-item.mega-menu-item-has-children:hover >ul.mega-sub-menu{
+      visibility: visible;
+  } 
+  .main-navigation #mega-menu-primary>li.mega-menu-item.mega-menu-item-has-children .mega-menu-column .mega-menu-item{
+    padding: 0!important
+  }
+  @media (min-width: 1200px){
+    .main-navigation #mega-menu-primary>li.mega-menu-item.mega-menu-item-has-children>.mega-sub-menu{
+          width: 88rem!important;
+        left: 26rem!important;
+        margin-top: 18px!important;
+        border-radius: 5px;
+        padding: 2rem 2rem 2.5rem!important;
+        border: 1px solid #ccc!important;
+        border-top: 4px solid #0caade!important;
+    }
+  }
+</style>
