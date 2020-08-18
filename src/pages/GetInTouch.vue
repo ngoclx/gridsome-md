4c6af60967e918c5e68824052e4ea79d2b27bcc8
Nguyen Ngoc Vinh <ngocvinh@DESKTOP-7K2GITQ.localdomain>
2020-08-18 11:49:25 +0700

    style get in touch

--- src/pages/GetInTouch.vue
+++ src/pages/GetInTouch.vue
@@ -23,75 +23,79 @@
               </div>
               <div class="col-lg-6 col-xl-7">
                 <div class="form enquiry" id="enquiry-form">
-                  <div class="row">
-                    <div class="col-auto">
-                      <p class="sub-title">
-                        LET’S GET STARTED
-                      </p>
-                      <h2>Project Enquiry</h2>
-                    </div>
-                  </div>
-
-                  <FormulateForm @submit="submitHandler">
+                  <div class="frm_forms">
                     <div class="row">
-                      <div class="col-6">
-                        <FormulateInput
-                          label="Name"
-                          name="fullname"
-                          validation="required"
-                          error-behavior="submit"
-                        />
-                      </div>
-                      <div class="col-6">
-                        <FormulateInput
-                          type="email"
-                          name="email"
-                          label="Email"
-                          validation="email"
-                        />
-                      </div>
-                      <div class="col-6">
-                        <FormulateInput
-                          name="phone"
-                          label="Phone"
-                          validation="required"
-                        />
-                      </div>
-                      <div class="col-6">
-                        <FormulateInput
-                          name="organization"
-                          label="Organization"
-                        />
-                      </div>
-                      <div class="col-12">
-                        <FormulateInput
-                          type="radio"
-                          label="Approximate Budget"
-                          validation="required"
-                          :options="{
-                            price0to5: '<$5K',
-                            price5to15: '$5K - $15K',
-                            price15to30: '$15K - $30K',
-                            price30to70: '$30K - $70K',
-                            price70up: '$70K >',
-                          }"
-                        />
-                      </div>
-
-                      <div class="col-12">
-                        <FormulateInput
-                          type="textarea"
-                          validation="required"
-                          name="services"
-                          label="Which services are you interested in?"
-                        />
-                      </div>
-
-                      <div class="col-12">
-                        <FormulateInput type="submit" label="Enquire Now" />
+                      
+                      <div class="col-auto">
+                        <p class="sub-title">
+                          LET’S GET STARTED
+                        </p>
+                        <h2>Project Enquiry</h2>
                       </div>
                     </div>
-                  </FormulateForm>
+                    <div class="with_frm_style">
+                      <FormulateForm @submit="submitHandler">
+                        <div class="row">
+                          <div class="col-6 form-group">
+                            <FormulateInput
+                              label="Name"
+                              name="fullname"
+                              validation="required"
+                              error-behavior="submit"
+                            />
+                          </div>
+                          <div class="col-6 form-group">
+                            <FormulateInput
+                              type="email"
+                              name="email"
+                              label="Email"
+                              validation="email"
+                            />
+                          </div>
+                          <div class="col-6 form-group">
+                            <FormulateInput
+                              name="phone"
+                              label="Phone"
+                              validation="required"
+                            />
+                          </div>
+                          <div class="col-6 form-group">
+                            <FormulateInput
+                              name="organization"
+                              label="Organization"
+                            />
+                          </div>
+                          <div class="col-12 form-group form-group-radio">
+                            <FormulateInput
+                              type="radio"
+                              label="Approximate Budget"
+                              validation="required"
+                              :options="{
+                                price0to5: '<$5K',
+                                price5to15: '$5K - $15K',
+                                price15to30: '$15K - $30K',
+                                price30to70: '$30K - $70K',
+                                price70up: '$70K >',
+                              }"
+                            />
+                          </div>
+
+                          <div class="col-12 form-group">
+                            <FormulateInput
+                              type="textarea"
+                              validation="required"
+                              name="services"
+                              label="Which services are you interested in?"
+                            />
+                          </div>
+
+                          <div class="col-12 form-group">
+                            <div class="frm_submit"><FormulateInput type="submit" label="Enquire Now" /></div>
+                          </div>
+                        </div>
+                      </FormulateForm>
+                      </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -233,20 +237,137 @@
 
     <latest-insights-block />
 
-    <b-modal id="highfive" hide-footer hide-header>
-      <h3>High Five</h3>
-      <p class="my-4">We’ll contact you soon to discuss your requirements.</p>
-      <p class="my-4">In the meantime, if you are up for a chat, feel free to call Mara on <g-link
+    <b-modal id="highfive" centered hide-footer hide-header>
+      <h2>High Five</h2>
+      <p>We’ll contact you soon to discuss your requirements.</p>
+      <p>In the meantime, if you are up for a chat, feel free to call Mara on <g-link
                     to="tel:+61415314198"
                     data-wpel-link="internal"
                     rel="noopener noreferrer"
                     >+61 415 314 198</g-link
                   ></p>
-      <b-button class="mt-3" block @click="gotoHome()">Close</b-button>
+      <b-button class="btn-close" block @click="gotoHome()"><i class="fa fa-times"></i></b-button>
     </b-modal>
 
   </Layout>
 </template>
+
+<style>
+  .modal-open .modal{
+    background-color: rgba(0,0,0,0.5);
+  }
+  .modal .modal-content{
+    border: none;
+  }
+  .modal h2{
+    font-size: 2.8rem;
+    color: #045499;
+    margin-bottom: 1.5rem;
+  }
+  .modal-custom{
+    background-color: rgba(0,0,0,0.5);
+  }
+  .modal-body{
+    padding: 3rem;
+  }
+  .enquiry .form-group{
+    margin-bottom: 2rem;
+  }
+  .enquiry .with_frm_style{
+    margin-top: 2rem;
+  }
+  .enquiry .formulate-input-label{
+      font-weight: 700;
+      font-size: 1.9rem;
+      color: #1d304a;
+      font-family: Nunito,sans-serif;
+      display: block;
+     float: none;
+     width: auto;
+  }
+  .enquiry .with_frm_style [data-is-showing-errors="true"] input,
+  .enquiry .with_frm_style [data-is-showing-errors="true"] textarea{
+    border: 1px solid #B94A48;
+  }
+  .enquiry .with_frm_style input[type=email], .enquiry .with_frm_style input[type=tel], .enquiry .with_frm_style input[type=text], .enquiry .with_frm_style textarea{
+    width: 100%;
+  }
+  .enquiry .with_frm_style textarea{
+    height: 15.4rem;
+  }
+  .enquiry .with_frm_style .frm_submit button {
+    background-image: -webkit-linear-gradient(303deg,#dc03ae 0,#da2e74 100%);
+    background-image: -o-linear-gradient(303deg,#dc03ae 0,#da2e74 100%);
+    background-image: linear-gradient(147deg,#dc03ae 0,#da2e74 100%);
+    background-image: -o-linear-gradient(147deg,#dc03ae 0,#da2e74 100%);
+    background-image: -webkit-linear-gradient(147deg,#dc03ae 0,#da2e74 100%);
+    background-image: -moz-linear-gradient(147deg,#dc03ae 0,#da2e74 100%);
+    color: #fff;
+    font-family: Nunito,sans-serif;
+  }
+  .enquiry  .form-group-radio .formulate-input .formulate-input-group{
+    display: flex;
+    display: -webkit-flex;
+  }
+  .enquiry  .form-group-radio .formulate-input .formulate-input-group .formulate-input-group-item{
+    margin-right: 1rem;
+  }
+  .enquiry  .form-group-radio .formulate-input .formulate-input-group .formulate-input-label {
+    position: relative;
+    color: #1d304a;
+    font-family: Nunito,sans-serif;
+    font-size: 1.9rem;
+    border-radius: 5px;
+    padding: .6rem 1.5rem;
+    display: block;
+    z-index: 1;
+    cursor: pointer;
+    border: 1px solid #c5e8f4;
+    background: rgba(12,170,222,.13);
+    border-radius: 5px;
+    font-weight: 400;
+}
+.enquiry  .form-group-radio .formulate-input-element--radio{
+  display: none;
+}
+.enquiry  .form-group-radio .formulate-input[data-has-value="true"] .formulate-input-group [data-has-value="true"] .formulate-input-label{
+    background-image: -webkit-linear-gradient(303deg,#0caade 0,#045499 100%);
+    background-image: -o-linear-gradient(303deg,#0caade 0,#045499 100%);
+    background-image: linear-gradient(147deg,#0caade 0,#045499 100%);
+    background-image: -o-linear-gradient(147deg,#0caade 0,#045499 100%);
+    background-image: -webkit-linear-gradient(147deg,#0caade 0,#045499 100%);
+    background-image: -moz-linear-gradient(147deg,#0caade 0,#045499 100%);
+    color: #fff;
+ }
+ .enquiry .formulate-input-errors{
+   list-style: none;
+   padding: 0;
+ }
+ .enquiry .formulate-input-errors .formulate-input-error{
+   color: #B94A48;
+   text-align: left;
+   margin-top: .5rem;
+    border-left: 10px solid #b94a48;
+    padding-left: .5rem;
+    font-family: Nunito,sans-serif;
+    font-weight: 700;
+    font-size: 1.4rem;
+ }
+ .btn-close{
+       position: absolute;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    font-size: 1.6rem;
+    right: -1rem;
+    top: -1rem;
+    background-color:#045499;
+    border: none;
+ }
+ .btn-close:hover,.btn-close:focus{
+   background-color: #dc03ae;
+ }
+</style>
 
 <script>
 import LatestInsightsBlock from "~/components/LatestInsightsBlock.vue"
