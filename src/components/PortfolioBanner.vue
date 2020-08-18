2528028035f12a5a5b41cd780ee1126f3abe912e
Nguyen Ngoc Vinh <ngocvinh@DESKTOP-7K2GITQ.localdomain>
2020-08-18 09:43:41 +0700

    add banner portfolio

---
+++ src/components/PortfolioBanner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="head-portfolio full-width">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-7">
+                <header class="entry-header">
+                    <h1 class="entry-title" itemprop="headline">{{ banner.title }}</h1>
+                </header>
+                <!-- .entry-header -->
+                <div class="industry-section-terms portfolio-terms pink">
+                    <span class="sr-only">Industry Section</span>
+                    <ul>
+                        <li v-for="item in banner.industrySectionTerms" :key="item.id">
+                            <g-link :to="'/industry-section/'+ item.slug" data-wpel-link="internal" rel="noopener noreferrer">{{ item.name }}</g-link>
+                        </li>
+                    </ul>
+                </div>
+                <div class="excerpt" v-html="banner.excerpt"></div>
+                <div class="service-provide-terms portfolio-terms blue">
+                    <span class="sr-only">Service provided</span>
+                    <ul>
+                        <li v-for="item in banner.serviceProvideTerms" :key="item.id">
+                            <g-link :to="'/service-provided/'+ item.slug" data-wpel-link="internal" rel="noopener noreferrer">{{ item.name }}</g-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-5">
+                <div class="feat-img">
+                  <g-image :src="banner.featuredMedia.sourceUrl"  :alt="banner.featuredMedia.altText" width="691" height="1024" class="attachment-large size-large wp-post-image" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "portfolioBanner",
+  props: {
+    banner: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods:{}
+};
+</script>
