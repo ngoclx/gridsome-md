227fa4f6416d755b2bd97c3fee31f80628c9ba4e
Nguyen Ngoc Vinh <ngocvinh@DESKTOP-7K2GITQ.localdomain>
2020-08-18 20:43:15 +0700

    add header mobile

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -20,6 +20,23 @@
         <navbar name="navigation-bar" /> 
 
       </div>
+      <div class="myHeaderWidgets">
+            <div class="d-xl-none mobile-logo">
+              <div class="site-logo"><g-image class="header-image"
+              :alt="$static.metadata.siteNam"
+              src="~/images/marameo-logo.svg"
+              :title="$static.metadata.siteNam"
+            /></div>
+          </div>
+          <g-link to="tel:1300811791" class="d-xl-none call-mobile button pink"
+                >1300 881 791</g-link>
+            <button class="menu-toggle d-flex d-xl-none justify-content-center align-items-center"
+                aria-controls="menu-mobile" aria-expanded="false">
+                <i class="fas fa-bars" aria-hidden="true"></i>
+                <i class="fas fa-times" aria-hidden="true"></i>
+                <span class=" sr-only mobile-menu">Menu</span>
+            </button>
+        </div>
     </header>
   </div>
 </template>
