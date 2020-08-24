ce929237db2c71f64a1015ee1b862aeae3c0c27e
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-08-24 14:33:26 +0700

    Vue loadscript module

--- src/main.js
+++ src/main.js
@@ -1,47 +1,16 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
-// import $ from 'jquery'
-// import jQuery from 'jquery'
-
-// import 'bootstrap/dist/css/bootstrap.min.css'
-//import '~/css/maxmegamenu/style.css'
-//import '~/style.css'
-
-// { src: "https://code.jquery.com/jquery-3.2.1.slim.min.js", async: true, defer: true },
-// { src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", async: true, defer: true },
-// { src: "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js", async: true, defer: true },
-// import 'https://code.jquery.com/jquery-3.2.1.slim.min.js'
-// import 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'
-// import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
-
-// import 'bootstrap'
-// import '~/js/calendar.js'
-
-// import 'jquery/dist/jquery.slim.min.js'
-// import 'popper.js/dist/popper.min.js'
-// import 'bootstrap/dist/js/bootstrap.min.js'
-// import jQuery from 'jquery'
-// import Popper from 'popper.js'
-// global.jQuery = jQuery;
-// global.Popper = Popper;
-
-// import jQuery from 'jquery'
-// if (typeof window != 'undefined') {
-//     window.jQuery = window.$ = jQuery
-// }
-
 import BootstrapVue from 'bootstrap-vue'
 import VueFormulate from '@braid/vue-formulate'
 import DefaultLayout from '~/layouts/Default.vue'
 import MenuItem from '~/components/MenuItem.vue'
+import LoadScript from 'vue-plugin-load-script';
 import _ from 'lodash'
 
 export default function(Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.use(BootstrapVue)
     Vue.use(VueFormulate)
-
+    Vue.use(LoadScript)
+    
     Vue.component('Layout', DefaultLayout)
     Vue.component('MenuItem', MenuItem)
 
