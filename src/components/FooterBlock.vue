ccebde34e925ccd9cdda8def3460fd2cd037b4b4
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-08-29 15:19:46 +0700

    Calendar block for client only

--- src/components/FooterBlock.vue
+++ src/components/FooterBlock.vue
@@ -1,141 +1,147 @@
 <template>
   <div>
-  <calendar-block v-if="!hideCalendar"/>
-  
-  <footer class="site-footer">
-    <div class="fat-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-xl-3">
-            <h2 id="insights-lastest-post-id">Insights</h2>
+    <ClientOnly>
+      <calendar-block v-if="!hideCalendar" />
+    </ClientOnly>
 
-            <div
-              class="insights-lastest-post"
-              aria-labelledby="insights-lastest-post-id"
-            >
-              <ul>
-                <li v-for="i in this.$static.latestInsights.edges" :key="i.key">
-                  <g-link :to="i.node.path">
-                    {{ i.node.title }}
-                  </g-link>
-                </li>
-              </ul>
+    <footer class="site-footer">
+      <div class="fat-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-xl-3">
+              <h2 id="insights-lastest-post-id">Insights</h2>
 
-              <g-link
-                class="text-uppercase read-more"
-                to="/all-insight"
-                data-wpel-link="internal"
-                rel="noopener noreferrer"
-                >Read all insights</g-link
+              <div
+                class="insights-lastest-post"
+                aria-labelledby="insights-lastest-post-id"
               >
-            </div>
-          </div>
-          <div class="col-md-6 col-xl-3">
-            <div class="footer-menu-item">
-              <h2 id="services-nav-id">Services</h2>
-              <nav aria-labelledby="services-nav-id">
                 <ul>
                   <li
-                    v-for="s in this.$static.servMenu.items"
-                    :key="s.key"
-                    class="menu-item"
+                    v-for="i in this.$static.latestInsights.edges"
+                    :key="i.key"
                   >
-                    <g-link :to="s.url | pathname">
-                      {{ removeSpecialCharacter(s.title) }}
+                    <g-link :to="i.node.path">
+                      {{ i.node.title }}
                     </g-link>
                   </li>
                 </ul>
-              </nav>
-            </div>
-          </div>
-          <div class="col-md-6 col-xl-3">
-            <div class="footer-menu-item">
-              <h2 id="technologies-nav-id">Technologies</h2>
-              <nav aria-labelledby="technologies-nav-id">
-                <ul>
-                  <li
-                    v-for="t in this.$static.techMenu.items"
-                    :key="t.key"
-                    class="menu-item"
-                  >
-                    <g-link :to="t.url | pathname">
-                      {{ removeSpecialCharacter(t.title) }}
-                    </g-link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div class="footer-menu-item">
-              <h2 id="migrations-nav-id">Migrations</h2>
-              <nav aria-labelledby="migrations-nav-id">
-                <ul>
-                  <li
-                    v-for="m in this.$static.migMenu.items"
-                    :key="m.key"
-                    class="menu-item"
-                  >
-                    <g-link :to="m.url | pathname">
-                      {{ removeSpecialCharacter(m.title) }}
-                    </g-link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-          <div class="col-md-6 col-xl-3">
-            <h2>Get in Touch</h2>
-            <div class="contacts">
-              <div class="contact-item">
-                <p><strong>G’day Mate! Sydney Office</strong></p>
-                <p>
-                  Level 2, 243 King St <br />
-                  Newtown, 2042 – Sydney
-                </p>
+
+                <g-link
+                  class="text-uppercase read-more"
+                  to="/all-insight"
+                  data-wpel-link="internal"
+                  rel="noopener noreferrer"
+                  >Read all insights</g-link
+                >
               </div>
-              <div class="contact-item">
-                <p><strong>Xin chao! Danang Office</strong></p>
-                <p>
-                  31 Tran Phu Street, <br />
-                  Hai Chau District - Danang
-                </p>
+            </div>
+            <div class="col-md-6 col-xl-3">
+              <div class="footer-menu-item">
+                <h2 id="services-nav-id">Services</h2>
+                <nav aria-labelledby="services-nav-id">
+                  <ul>
+                    <li
+                      v-for="s in this.$static.servMenu.items"
+                      :key="s.key"
+                      class="menu-item"
+                    >
+                      <g-link :to="s.url | pathname">
+                        {{ removeSpecialCharacter(s.title) }}
+                      </g-link>
+                    </li>
+                  </ul>
+                </nav>
               </div>
-              <div class="contact-item">
-                <p><strong>Ciao Belli! Rome Office</strong></p>
-                <p>
-                  Via delle Baleniere, 186 <br />
-                  Ostia Lido, 00121 - Roma
-                </p>
+            </div>
+            <div class="col-md-6 col-xl-3">
+              <div class="footer-menu-item">
+                <h2 id="technologies-nav-id">Technologies</h2>
+                <nav aria-labelledby="technologies-nav-id">
+                  <ul>
+                    <li
+                      v-for="t in this.$static.techMenu.items"
+                      :key="t.key"
+                      class="menu-item"
+                    >
+                      <g-link :to="t.url | pathname">
+                        {{ removeSpecialCharacter(t.title) }}
+                      </g-link>
+                    </li>
+                  </ul>
+                </nav>
               </div>
-              <g-link to="tel:1300811791" class="button pink"
-                >1300 881 791</g-link
-              >
+              <div class="footer-menu-item">
+                <h2 id="migrations-nav-id">Migrations</h2>
+                <nav aria-labelledby="migrations-nav-id">
+                  <ul>
+                    <li
+                      v-for="m in this.$static.migMenu.items"
+                      :key="m.key"
+                      class="menu-item"
+                    >
+                      <g-link :to="m.url | pathname">
+                        {{ removeSpecialCharacter(m.title) }}
+                      </g-link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <div class="col-md-6 col-xl-3">
+              <h2>Get in Touch</h2>
+              <div class="contacts">
+                <div class="contact-item">
+                  <p><strong>G’day Mate! Sydney Office</strong></p>
+                  <p>
+                    Level 2, 243 King St <br />
+                    Newtown, 2042 – Sydney
+                  </p>
+                </div>
+                <div class="contact-item">
+                  <p><strong>Xin chao! Danang Office</strong></p>
+                  <p>
+                    31 Tran Phu Street, <br />
+                    Hai Chau District - Danang
+                  </p>
+                </div>
+                <div class="contact-item">
+                  <p><strong>Ciao Belli! Rome Office</strong></p>
+                  <p>
+                    Via delle Baleniere, 186 <br />
+                    Ostia Lido, 00121 - Roma
+                  </p>
+                </div>
+                <g-link to="tel:1300811791" class="button pink"
+                  >1300 881 791</g-link
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="bottom-section">
-      <div class="container">
-        <div class="row">
-          <div class="copy col-lg-8">
-            <p>
-              Digital Agency in Sydney specialising in web design, web
-              development, user experience, accessibility and more. © {{ currentYear() }}
-              Marameo Design Pty Ltd.
-            </p>
-          </div>
-          <div class="terms-and-policy col-lg-4">
-            <p>
-              <g-link to="/privacy-policy">Privacy Policy</g-link> |
-              <g-link to="/terms-of-use-of-website">Terms of Use</g-link> |
-              <g-link to="/sitemap">Sitemap</g-link>
-            </p>
+      <div class="bottom-section">
+        <div class="container">
+          <div class="row">
+            <div class="copy col-lg-8">
+              <p>
+                Digital Agency in Sydney specialising in web design, web
+                development, user experience, accessibility and more. ©
+                {{ currentYear() }}
+                Marameo Design Pty Ltd.
+              </p>
+            </div>
+            <div class="terms-and-policy col-lg-4">
+              <p>
+                <g-link to="/privacy-policy">Privacy Policy</g-link> |
+                <g-link to="/terms-of-use-of-website">Terms of Use</g-link> |
+                <g-link to="/sitemap">Sitemap</g-link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   </div>
 </template>
 
@@ -183,33 +189,31 @@ import CalendarBlock from "~/components/CalendarBlock.vue";
 export default {
   name: "FooterBlock",
   components: {
-     CalendarBlock,
+    CalendarBlock,
   },
   props: {
-    hideCalendar:{
+    hideCalendar: {
       type: Boolean,
       required: false,
-    }
+    },
   },
   filters: {
-    pathname: function (value) {
-      var url = new URL(value)
-      return url.pathname
-    }
+    pathname: function(value) {
+      var url = new URL(value);
+      return url.pathname;
+    },
   },
   methods: {
-    currentYear(){
+    currentYear() {
       var d = new Date();
       var year = d.getFullYear();
-      return year
+      return year;
     },
     removeSpecialCharacter(str) {
-        return str.replace(/[^&a-zA-Z ]/g, '')
+      return str.replace(/[^&a-zA-Z ]/g, "");
     },
   },
-  computed: {
-      
-  },
+  computed: {},
   mounted() {
     // console.log(this.$static.migMenu)
     // console.log(this.$static.latestInsights)
