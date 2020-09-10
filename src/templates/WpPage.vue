cacd8d55daeeb45941749bdf86371b9162c5a123
Ly Xuan Ngoc <xngocly.tq@gmail.com>
2020-09-10 22:21:51 +0700

    Fix

--- src/templates/WpPage.vue
+++ src/templates/WpPage.vue
@@ -2,7 +2,6 @@
   <Layout :path="$page.wpPage.path" :ptitle="$page.wpPage.title">
     <div id="page" class="hfeed site grid-container container grid-parent">
       <div id="content" class="site-content">
-        {{ $page.wpPage.id }}
         <h1 class="d-none" v-html="$page.wpPage.title" />
 
         <div v-html="$page.wpPage.content" />
